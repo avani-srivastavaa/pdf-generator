d046488417c0fded567f4ff09b7a2dad5dbefebd
Avani Srivastava <avanisrivastava16@gmail.com>
2025-07-07 22:29:44 +0530

    Update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -8,10 +8,9 @@ db_url = os.getenv("DATABASE_URL")
 
 admin_user = os.getenv("ADMIN_USERNAME")
 admin_password = os.getenv("ADMIN_PASSWORD")
+if not admin_password:
+    raise ValueError("ADMIN_PASSWORD environment variable not set")
 admin_pass_hash = generate_password_hash(admin_password)
-
-print("Loaded admin user:", admin_user)
-print("Loaded admin password:", admin_password)
 
 def check_login(username, password):
     return username == admin_user and check_password_hash(admin_pass_hash, password)
