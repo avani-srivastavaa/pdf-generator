c8d348d4d88140ab4751562e5d06181d78875aa2
avani-srivastavaa <avanisrivastava16@gmail.com>
2025-07-08 14:48:31 +0530

    Render deployment

--- backend/db.py
+++ backend/db.py
@@ -8,7 +8,10 @@ load_dotenv()
 # Connection function
 def get_connection():
     db_url = os.getenv("DATABASE_URL")
+    if not db_url:
+        raise ValueError("DATABASE_URL environment variable is not set.")
     return psycopg2.connect(db_url)
+
 
 # Fetch certificate by UID
 def get_certificate_by_uid(uid):
